0f68d1fba8368fa6db563584c4bdf5621bdd4702
TROILLARD Romain <133049750+MrPaquiteau@users.noreply.github.com>
2025-04-29 19:27:01 +0200

    refactor: remove useless files for pages & actions

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import os
-import webbrowser
-from socketserver import TCPServer
-from http.server import SimpleHTTPRequestHandler
 from src.fetch_teams_api import run as fetch_teams_api
 from src.fetch_matches_api import run as fetch_matches_api
 from src.fetch_players_api import run as fetch_players_api
@@ -20,34 +17,11 @@ def fetch_data_from_selenium():
     fetch_players_selenium()
     fetch_matches_selenium()
 
-def open_html_file():
-    PORT = 8000
-
-    url = f"http://localhost:{PORT}/docs/index.html"
-
-    with TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
-        print(f"Serving on {url}")
-        webbrowser.open(url)
-        try:
-            httpd.serve_forever()
-        except KeyboardInterrupt:
-            print("\nShutting down server.")
-            httpd.shutdown()
-
 def main():
-    if not os.path.exists("data"):
-        os.makedirs("data")
-    action = input("What do you want to do? (1) Fetch data from API, (2) Fetch data from Selenium, (3) Make HTML, (4) Open website: ")
-    if action == "1":
-        fetch_data_from_api()
-    elif action == "2":
-        fetch_data_from_selenium()
-    elif action == "3":
-        build()
-    elif action == "4":
-        open_html_file()
-    else:
-        print("Invalid action")
+    if not os.path.exists("docs/data"):
+        os.makedirs("docs/data")
+    fetch_data_from_api()
+    build()
 
 if __name__ == '__main__':
     main()
